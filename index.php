d8f1886e9802c92e74a085ef5ba876def17c0711
nuzulkaromah <nuzulkaromah@gmail.com>
2024-04-03 14:34:46 +0700

    Menambahakan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque fugit a ipsa praesentium minima cupiditate obcaecati alias illo. Maiores commodi nulla recusandae reprehenderit nobis. Minus magnam alias quos animi maiores.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
